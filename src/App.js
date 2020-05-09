6f393d40d01727a44b24e93ccec0fb06b74a2b66
nd98 <bptpgt@gmail.com>
2020-05-09 16:28:16 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactAudioPlayer from "react-audio-player";
+import YouTube from "react-youtube";
+import "./App.css";
 
 function App() {
+  const links = {
+    "Sao Paulo": "WcjGyIjHLDA",
+    "New York": "MeZ8P73JNtw",
+  };
+  const cities = ["Sao Paulo", "New York"];
+  const [selectedCity, setSelectedCity] = useState(cities[1]);
+  const [isNoiseMuted, setIsNoiseMuted] = useState(false);
+
+  const speed = 0.5;
+  const opts = {
+    height: "100vh",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+      fs: 0,
+      disableKeyboard: 1,
+      controls: 0,
+      showinfo: 0,
+      modestbranding: 0,
+      rel: 0,
+    },
+  };
+  console.log();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ height: "100vh" }}>
+        <YouTube
+          videoId={links[selectedCity]}
+          opts={opts}
+          onPlaybackRateChange={() => {}}
+        />
+        <ReactAudioPlayer
+          src="http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg"
+          autoPlay
+          controls
+          muted={isNoiseMuted}
+        />
+        <select
+          style={{ position: "absolute", top: 10 }}
+          value={selectedCity}
+          name="city"
+          onChange={(event) => setSelectedCity(event.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          {cities.map((city) => (
+            <option value={city}>{city}</option>
+          ))}
+        </select>
+        <button
+          style={{ position: "absolute", top: 10, right: 270 }}
+          onClick={() => setIsNoiseMuted(!isNoiseMuted)}
+        >
+          Mute Radio
+        </button>
+      </div>
     </div>
   );
 }
